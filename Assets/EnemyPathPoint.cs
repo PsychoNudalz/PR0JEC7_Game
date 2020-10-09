3740b21e2842e885b7bfa05f594b3bf21c678d4c
Daniel <Newsom>
2020-10-09 17:59:53 +0100

    Danny - Chicken following path with walking animation

---
+++ Assets/EnemyPathPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathPoint : MonoBehaviour
+{
+    public string action = "Continue";
+    // Start is called before the first frame update
+
+    public string GetAction(){
+        return this.action;
+    }
+}
