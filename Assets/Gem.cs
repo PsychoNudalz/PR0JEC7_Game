f17774af8f81fa612fa523ee97fa95df47c7ff78
Daniel <Newsom>
2020-10-10 15:13:36 +0100

    Danny - push for merge

--- Assets/Gem.cs
+++ Assets/Gem.cs
@@ -6,10 +6,36 @@ public class Gem : MonoBehaviour
 {
     public GemCollection gemCollection;
 
+    public Vector3 rotationAngle;
+    public float rotationSpeed;
+
+    public float floatSpeed;
+    private bool goingUp = true;
+    public float floatRate;
+    private float floatTimer;
+
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(rotationAngle * rotationSpeed * Time.deltaTime);
+
+        floatTimer += Time.deltaTime;
+            Vector3 moveDir = new Vector3(0.0f, 0.0f, floatSpeed);
+            transform.Translate(moveDir);
+
+            if (goingUp && floatTimer >= floatRate)
+            {
+                goingUp = false;
+                floatTimer = 0;
+                floatSpeed = -floatSpeed;
+            }
+
+            else if(!goingUp && floatTimer >= floatRate)
+            {
+                goingUp = true;
+                floatTimer = 0;
+                floatSpeed = +floatSpeed;
+            }
     }
 
     private void OnTriggerEnter(Collider other){
