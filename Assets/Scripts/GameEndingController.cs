3892f7687a96962fc363db0c8c4153b60411b1f5
fceek <fceek96@gmail.com>
2020-10-27 04:33:30 +0000

    Created game ending UI, updated build settings / scene

---
+++ Assets/Scripts/GameEndingController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEndingController : MonoBehaviour
+{
+    GameObject player;
+    GameManager gameManager;
+    public GameObject endingType;
+
+
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerMasterHandlerScript>().gameObject;
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject == player) {
+            if (gameManager.allGemsCollected()) {
+                endingType.SetActive(true);
+            }
+        }
+    }
+
+    public void NextLevelOnClick() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ReturnToMainOnClick() {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void ExitOnClick() {
+        Application.Quit();
+    }
+}
