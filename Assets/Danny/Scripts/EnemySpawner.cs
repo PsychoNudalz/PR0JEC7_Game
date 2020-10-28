11fbd0e9bc4ce013db3503f4c3630b17d8706e71
Daniel <Newsom>
2020-10-28 18:20:01 +0000

    Danny - Push for merge

--- Assets/Danny/Scripts/EnemySpawner.cs
+++ Assets/Danny/Scripts/EnemySpawner.cs
@@ -2,21 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Enemy {Chicken, Skeleton };
 public class EnemySpawner : MonoBehaviour
 {
-    public IEnumerator RespawnEnemyChicken(GameObject prefab, Vector3 position, float delay, Transform waypoints)
-    {
-        Debug.Log("Respawning..." + prefab.ToString() + position.ToString() + delay + waypoints.ToString());
-        yield return new WaitForSecondsRealtime(delay);
-        Debug.Log("Done");
-        GameObject respawn = Instantiate(prefab, position, Quaternion.identity);
-        respawn.GetComponent<ChickenAgent>().waypointsToFollow = waypoints;
+    [SerializeField]
+    private GameObject chickenPrefab;
+    [SerializeField]
+    private GameObject skeletonPrefab;
 
-    }
-
-    public void SpawnChicken(GameObject prefab, Vector3 position, float delay, Transform waypoints)
+    public void SpawnEnemy(Enemy enemyType, Vector3 position, float delay, Transform waypoints)
     {
+        if(enemyType.Equals(Enemy.Chicken)){
+            StartCoroutine(RespawnChicken(position, delay, waypoints));
+        }
         
     }
+
+    public IEnumerator RespawnChicken(Vector3 position, float delay, Transform waypoints)
+    {
+        Debug.Log("Respawning Chicken..." + position + "  -  " + delay + "  -  " + waypoints);
+        yield return new WaitForSecondsRealtime(delay);
+        GameObject respawn = Instantiate(chickenPrefab, position, Quaternion.identity);
+        respawn.GetComponent<ChickenAgent>().waypointsToFollow = waypoints;
+        respawn.GetComponent<ChickenAgent>().enabled = true;
+        respawn.GetComponent<ChickenAgent>().ResetChicken();
+        Debug.Log("Done");
+    }
+
      
 }
